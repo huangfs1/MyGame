011193f8cabd183646f9aa0e54de4fb330f0b5f4
huangfs <1430569268@qq.com>
2023-04-25 23:05:43 +0800

    first commit

---
+++ Assets/Excel/Runtime/ExcelManager.Tools.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Excel
+{
+    public partial class ExcelManager
+    {
+        /// <summary>
+        /// 反射获取接口类型的实现类
+        /// </summary>
+        public static IEnumerable<Type> GetInterfaceType(Type interfaceType)
+        {
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (var type in assembly.GetTypes())
+                {
+                    foreach (var t in type.GetInterfaces())
+                    {
+                        if (t == interfaceType)
+                        {
+                            yield return type;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
